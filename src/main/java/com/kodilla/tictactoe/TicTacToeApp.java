a5bd0d3ec11d057803e4f18ca2f32b4816aa2e81
Szymon Wizdorf <szymekwizdorf@gmail.com>
2021-12-05 20:41:28 +0100

    TicTacToe 1.0.3

--- src/main/java/com/kodilla/tictactoe/TicTacToeApp.java
+++ src/main/java/com/kodilla/tictactoe/TicTacToeApp.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.input.MouseButton;
 import javafx.scene.layout.*;
@@ -13,14 +14,13 @@ import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-import java.awt.*;
 
 
 public class TicTacToeApp extends Application {
 
     private Image imageback = new Image("file:src/main/resources/pencil.png");
     private boolean turnX=true;
-
+    private int counter=0;
     public static void main(String[] args) {
         launch(args);
     }
@@ -30,50 +30,60 @@ public class TicTacToeApp extends Application {
         BackgroundSize backgroundSize = new BackgroundSize(100, 100, true, true, true, false);
         BackgroundImage backgroundImage = new BackgroundImage(imageback, BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize);
         Background background = new Background(backgroundImage);
+        Button button= new Button();
+        button.setText("New Game");
 
         GridPane grid = new GridPane();
         grid.setBackground(background);
+        grid.getChildren().add(button);
 
-        Scene scene = new Scene(grid, 600, 600, Color.WHITE);
-        for(int i=0; i<3;i++){
+        Scene scene = new Scene(grid, 600, 800, Color.BLACK);
+        for(int i=1; i<4;i++){
             for(int j=0; j<3; j++){
-                Title title= new Title();
-                title.setTranslateX(j*200);
-                title.setTranslateY(i*200);
-                grid.getChildren().add(title);
+                Tile tile= new Tile();
+                tile.setTranslateX(j*200);
+                tile.setTranslateY(i*200);
+                grid.getChildren().add(tile);
+
             }
         }
-
-
-
         primaryStage.setTitle("Tic-Tac-Toe");
         primaryStage.setScene(scene);
         primaryStage.show();
+
+        button.setOnMouseClicked(event->{
+            primaryStage.close();
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        });
     }
 
 
-    public class Title extends StackPane{
+    public class Tile extends StackPane{
         private Text text= new Text();
-        Rectangle border= new Rectangle(200,200);
-        public Title(){
 
+        public Tile(){
 
+            Rectangle border= new Rectangle(200,200);
             border.setFill(null);
             border.setStroke(Color.BLACK);
             text.setFont(Font.font(72));
 
             setAlignment(Pos.CENTER);
-            getChildren().addAll(border, text);
+
 
             setOnMouseClicked(event-> {
                         if (!turnX)
                             return;
-                        drawX();
-                        turnX = false;
-                        computerMovement();
+                        else {
+                            drawX();
+                            turnX = false;
+                            counter++;
+                            computerMovement();
+
+                        }
                     });
-
-
+            getChildren().addAll(border,text);
         }
 
         private void drawX(){
@@ -84,12 +94,12 @@ public class TicTacToeApp extends Application {
         }
         private void computerMovement(){
             if(!turnX) {
-
-                setTranslateX(0);
-                setTranslateY(200);
-                draw0();
+                System.out.println("Now computer move.");
+                turnX=true;
+                if(counter==5)
+                    System.exit(0);
             }
-            turnX=true;
+
         }
     }
 }
